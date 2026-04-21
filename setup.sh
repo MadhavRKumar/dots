f81a2c44b7ff1d4c832bc3e55696465cd3820d14
MadhavRKumar <madhavrkumar@gmail.com>
2026-04-20 22:37:49 -0400

    add dry run feature

--- setup.sh
+++ setup.sh
@@ -3,11 +3,57 @@
 # Script to choose the OS and install the dot files by creating symbolic links
 
 # Argument allows for dry run of the script to ensure that the symbolic links will be created correctly without actually creating them
-
 DRY_RUN=false
 if [ "$1" == "--dry-run" ]; then
-    DRY_RUN=true
+    create_symlink() {
+        local source_file="$1"
+        local target_file="$2"
+
+        # if the directory of the target file does not exist, create it
+        if [ ! -d "$(dirname "$target_file")" ]; then
+            echo "Would create directory: $(dirname "$target_file")"
+        fi
+
+        # Check if the source file exists
+        if [ ! -e "$source_file" ]; then
+            echo "Source $source_file does not exist. Skipping."
+            return
+        fi
+
+        # Check if the target file already exists. If it does, create< a backup of it by renaming it with a .backup extension   
+        if [ -e "$target_file" ]; then
+            echo "Would backup existing file: $target_file to $target_file.backup"
+        fi
+
+        echo "Would create symbolic link for $source_file at $target_file."
+    }
+else
+    create_symlink() {
+        local source_file="$1"
+        local target_file="$2"
+
+        # if the directory of the target file does not exist, create it
+        if [ ! -d "$(dirname "$target_file")" ]; then
+            mkdir -p "$(dirname "$target_file")"
+        fi
+
+        # Check if the source file exists
+        if [ ! -e "$source_file" ]; then
+            echo "Source $source_file does not exist. Skipping."
+            return
+        fi
+
+        # Check if the target file already exists. If it does, create< a backup of it by renaming it with a .backup extension   
+        if [ -e "$target_file" ]; then
+            mv "$target_file" "$target_file.backup"
+            echo "Backed up existing file: $target_file to $target_file.backup"
+        fi
+
+        ln -s "$source_file" "$target_file"
+        echo "Created symbolic link for $source_file at $target_file."
+    }
 fi
+
 
 # Prompt the user to choose the OS
 echo "Choose your OS:"
@@ -21,39 +67,6 @@ SOURCE_DIR="$(dirname "$(realpath "$0")")"
 # Set the target directory for the dot files
 TARGET_DIR="$HOME"
 
-# Function to create symbolic links
-create_symlink() {
-    local source_file="$1"
-    local target_file="$2"
-
-    # if the directory of the target file does not exist, create it
-    if [ ! -d "$(dirname "$target_file")" ]; then
-
-        if [ "$DRY_RUN" = true ]; then
-            echo "Would create directory: $(dirname "$target_file")"
-        else
-            mkdir -p "$(dirname "$target_file")"
-        fi
-    fi
-
-    # Check if the source file exists
-    if [ ! -e "$source_file" ]; then
-        echo "Source $source_file does not exist. Skipping."
-        return
-    fi
-
-    # Check if the target file already exists    
-    if [ -e "$target_file" ]; then
-        echo "Target $target_file already exists. Skipping $source_file."
-    else
-        if [ "$DRY_RUN" = true ]; then
-            echo "Would create symbolic link for $source_file at $target_file."
-            return
-        fi
-        ln -s "$source_file" "$target_file"
-        echo "Created symbolic link for $source_file at $target_file."
-    fi
-}
 
 # Install the dot files based on the chosen OS
 case $os_choice in
